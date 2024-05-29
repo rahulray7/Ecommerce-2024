c789daa44a282f152a2f822c427f2d3372dd6ad9
rahulray7 <103201189+rahulray7@users.noreply.github.com>
2024-05-30 01:39:07 +0530

    final code

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { categoryControlller, createCategoryController, deleteCategoryCOntroller, singleCategoryController, updateCategoryController } from './../controllers/categoryController.js';
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+const router = express.Router();
+
+router.post(
+    '/create-category',
+    requireSignIn,
+    isAdmin,
+    createCategoryController
+  );
+
+router.put(
+    "/update-category/:id",
+    requireSignIn,
+    isAdmin,
+    updateCategoryController
+  ); 
+
+router.get("/get-category", categoryControlller);  
+
+router.get("/single-category/:slug", singleCategoryController);
+
+router.delete(
+    "/delete-category/:id",
+    requireSignIn,
+    isAdmin,
+    deleteCategoryCOntroller
+  );
+
+export default router  
